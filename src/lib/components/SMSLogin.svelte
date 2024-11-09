b0dd11064cea54a079c2b7b2dc38f0cf50c7610c
Vizitys <53524661+Vizitys@users.noreply.github.com>
2024-11-09 13:35:33 +0200

    basic verification code sending

--- src/lib/components/SMSLogin.svelte
+++ src/lib/components/SMSLogin.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-	import * as Card from "$lib/components/ui/card/index.js";
-	import { Input } from "$lib/components/ui/input/index.js";
-	import Button from "./ui/button/button.svelte";
-
-	let phonenum = $state('');
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import Button from './ui/button/button.svelte';
 </script>
 
 <Card.Root>
-	<Card.Header>
-		<Card.Title>Please Authenticate</Card.Title>
-	</Card.Header>
-	<Card.Content>
-		<Input bind:value={phonenum} type="tel" placeholder="+358 12 3456789" />
-	</Card.Content>
-	<Card.Footer>
-		<Button>Send SMS</Button>
-	</Card.Footer>
+	<form method="POST" action="/sms?/send">
+		<Card.Header>
+			<Card.Title>Please Authenticate</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<label for="phonenum"
+				>Phone number
+				<Input name="to" type="tel" placeholder="+358 12 3456789" />
+			</label>
+		</Card.Content>
+		<Card.Footer>
+			<Button type="submit">Send SMS</Button>
+		</Card.Footer>
+	</form>
 </Card.Root>
