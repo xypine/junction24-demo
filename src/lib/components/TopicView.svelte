57e6f85ae9206a6567843b74ee0cf4a4b314e36a
jeherber <avitusti@gmail.com>
2024-11-09 23:08:09 +0200

    viewvote ja topic view

---
+++ src/lib/components/TopicView.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+    let title = "This is title";
+    let description = "This is description";
+    const data = {
+    id: 1111,
+    created_at: "10:20",
+    updated_at: "10:21",
+    creator_id: 2222,
+    share_slug: "testisivu",
+    topic: "wopic",
+    description: "aaa",
+    participants_can_submit_comments: true,
+    show_explanations: true,
+    prompt_subscription: true,
+    show_unapproved_comments: true,
+}
+</script>
+
+<Card.Root class="w-10/12">
+    <Card.Header>
+        <Card.Title>{data.topic}</Card.Title>
+    </Card.Header>
+    <Card.Content>
+        <p>{data.description}</p>
+    </Card.Content>
+</Card.Root>
+
+
+
+<style>
+    .padd{
+        padding-left: 5px;
+        padding-right: 5px;
+    }
+</style>
